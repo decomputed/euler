fb4c8da4a0a0dd5de2415ce72392a3195bad5466
Luis <luis@decomputed.com>
2015-02-23 09:20:15 +0100

    Brought back the spec for primes

---
+++ test/Numeric/Euler/PrimesSpec.hs
@@ -0,0 +1,13 @@
+module Numeric.Euler.PrimesSpec (main, spec) where
+
+import Test.Hspec
+import Numeric.Euler.Primes
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "isPrime" $ do
+    it "works" $ do
+      isPrime2 7 `shouldBe` True
