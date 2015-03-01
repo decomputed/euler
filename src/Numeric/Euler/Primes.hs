0e24cd863494b375661fa55864e5bf5d3b64ca3e
Luis <luis@decomputed.com>
2015-03-01 13:00:58 +0100

    Fixed all -Wall errors on compilation

--- src/Numeric/Euler/Primes.hs
+++ src/Numeric/Euler/Primes.hs
@@ -1,6 +1,9 @@
 module Numeric.Euler.Primes (
   isPrime2,
-  trialAndDivision2
+  trialAndDivision2,
+  erastothenes3,
+  sundaram5,
+  atkin1
   ) where
 
 import Data.List (sort)
@@ -15,15 +18,17 @@ isPrime2 n
   | n == 2    = True
   | otherwise = not (any (\i -> n `mod` i == 0) [3..n-1])
 
+trialAndDivision2 :: Int -> [Int]
 trialAndDivision2 limit = 2:3:5:[n | n <- [7,9..limit], isPrime2 n]
 
 
 {-------------------------------------------------
 -------------------------------------------------}
 
-
+erastothenes3 :: Int -> [Int]
 erastothenes3 limit = eSieve3 [3,5..limit] [2]
 
+eSieve3 :: [Int] -> [Int] -> [Int]
 eSieve3 [] primes     = primes
 eSieve3 (x:xs) primes = eSieve3 ([y | y <- xs, y `mod` x /= 0]) (x:primes)
 
@@ -33,14 +38,16 @@ eSieve3 (x:xs) primes = eSieve3 ([y | y <- xs, y `mod` x /= 0]) (x:primes)
 
 initialSundaramSieve :: Int -> [Int]
 initialSundaramSieve limit =
-  let topi = floor (sqrt ((fromIntegral limit) / 2)) in
+  let topi = floor ((sqrt ((fromIntegral limit) / 2)) :: Double) in
   [i + j + 2 * i * j | i <- [1..topi],
-   j <- [i..floor((fromIntegral(limit-i)) / fromIntegral(2*i+1))]]
+   j <- [i..floor (((fromIntegral(limit-i)) / fromIntegral(2*i+1)) :: Double)]]
 
+sundaram5 :: Int -> [Int]
 sundaram5 limit =
-  let halfLimit = floor((limit / 2)-1) in
+  let halfLimit = floor( (((fromIntegral limit) / 2)-1) :: Double) in
   2:removeComposites ([1..halfLimit]) (sort $ initialSundaramSieve halfLimit) 
 
+removeComposites :: [Int] -> [Int] -> [Int]
 removeComposites [] _ = []
 removeComposites (n:ns) [] = (2*n+1) : (removeComposites ns [])
 removeComposites (n:ns) (c:cs)
@@ -64,8 +71,10 @@ initialAtkinSieve limit =
 
 
 aFlip :: (Int,Int) -> (Int,Int)
-aFlip (x,1) = (x,0)
-aFlip (x,0) = (x,1)
+aFlip (x,y)
+  | y == 0        = (x,1)
+  | y == 1        = (x,0)
+  | otherwise     = error "Needs zero or one"
 
 
 flipAll :: [Int] -> [(Int, Int)] -> [(Int, Int)]
@@ -79,9 +88,9 @@ flipAll (f:fs) ((s,b):ss)
 
 firstStep :: Int ->  [(Int,Int)] ->  [(Int,Int)]
 firstStep size sieve =
-  let topx = floor(sqrt(fromIntegral (size `div` 4)))
-      topy = floor(sqrt(fromIntegral size)) in
-  flipAll (sort [n | n <- [4*x^2 + y^2| x <- [1..topx],
+  let topx = floor( (sqrt(fromIntegral (size `div` 4))) :: Double)
+      topy = floor( (sqrt(fromIntegral size)) :: Double) in
+  flipAll (sort [n | n <- [4*x^(2::Integer) + y^(2::Integer)| x <- [1..topx],
                            y <- [1,3..topy]],
                  n `mod` 60 `elem` [1,13,17,29,37,41,49,53]])
   sieve
@@ -89,9 +98,9 @@ firstStep size sieve =
 
 secondStep :: Int ->  [(Int,Int)] ->  [(Int,Int)]
 secondStep size sieve =
-  let topx = floor(sqrt(fromIntegral (size `div` 3)))
-      topy = floor(sqrt(fromIntegral size)) in
-  flipAll (sort [n | n <- [3*x^2 + y^2 | x <- [1,3..topx],
+  let topx = floor( (sqrt(fromIntegral (size `div` 3))) :: Double)
+      topy = floor( (sqrt(fromIntegral size)) :: Double) in
+  flipAll (sort [n | n <- [3*x^(2::Integer) + y^(2::Integer) | x <- [1,3..topx],
                            y <- [2,4..topy]],
                  n `mod` 60 `elem` [7,19,31,43]])
   sieve
@@ -99,19 +108,20 @@ secondStep size sieve =
 
 thirdStep :: Int ->  [(Int,Int)] ->  [(Int,Int)]
 thirdStep size sieve =
-  let topx = floor(sqrt(fromIntegral size)) in
-  flipAll (sort [n | n <- [3*x^2 - y^2 | x <- [1..topx],
+  let topx = floor( (sqrt(fromIntegral size)) :: Double) in
+  flipAll (sort [n | n <- [3*x^(2::Integer) - y^(2 :: Integer) | x <- [1..topx],
                            y <- [(x-1),(x-3)..1],
                            x > y],
                  n `mod` 60 `elem` [11,23,47,59]])
   sieve
 
-
+unmarkMultiples :: Int -> Int -> [(Int,Int)] -> [(Int,Int)]
 unmarkMultiples limit n sieve =
   let nonPrimes = [y | y <-[n,n+n..limit]] in
   unmarkAll nonPrimes sieve
 
 
+unmarkAll :: [Int] -> [(Int,Int)] -> [(Int, Int)]
 unmarkAll _        []    = []
 unmarkAll []       sieve = sieve
 unmarkAll (np:nps) ((s,b):ss) 
@@ -125,7 +135,8 @@ atkin1 limit =
   aSieve1 limit ((thirdStep limit) . (secondStep limit) . (firstStep limit) $ initialAtkinSieve limit) [(5,1),(3,1),(2,1)]
 
 
+aSieve1 :: Int -> [(Int,Int)] ->[(Int,Int)] -> [(Int,Int)]
 aSieve1 _     []         primes = primes
 aSieve1 limit ((x,b):xs) primes
-  | b == 1 = aSieve1 limit (unmarkMultiples limit (x^2) xs) ((x,b): primes)
+  | b == 1 = aSieve1 limit (unmarkMultiples limit (x^(2::Integer)) xs) ((x,b): primes)
   | otherwise = aSieve1 limit xs primes
