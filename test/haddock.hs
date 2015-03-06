d42f8143275c4cce4547948aef5f844ee7caaeae
Luis <luis@decomputed.com>
2015-03-06 12:37:21 +0100

    Made script to run documentation coverage

---
+++ test/haddock.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import Data.List (genericLength)
+import Data.Maybe (catMaybes)
+import System.Exit (exitFailure, exitSuccess)
+import System.Process (readProcess)
+import Text.Regex
+import Text.Regex.Posix
+
+expected :: Fractional a => a
+expected = 85
+
+readMatchAsInt :: [String] -> Float
+readMatchAsInt list = (read (list !! 1) :: Float)
+
+coverageOk :: [Float] -> Bool
+coverageOk values = (realToFrac (sum values) / genericLength values) > expected
+--coverageOk = all (> expected)
+
+main :: IO ()
+main = do
+  output <- readProcess "cabal" ["haddock"] ""
+  let percents = map readMatchAsInt (output =~ "^ *([0-9]*)% " :: [[String]]) in
+   if coverageOk percents
+       then exitSuccess
+       else putStr output >> exitFailure
+
