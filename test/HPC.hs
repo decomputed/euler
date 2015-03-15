e96e36238b8fc834d822d29cfadc54a33d9474d0
Luis <luis@decomputed.com>
2015-03-15 16:00:00 +0100

    Now running HPC as well as part of the runhaskell kind of build

--- test/HPC.hs
+++ test/HPC.hs
@@ -13,6 +13,12 @@ expected = 85
 simpleQName :: String -> QName
 simpleQName name = QName{qName=name, qURI=Nothing, qPrefix=Nothing}
 
+paramsForCabal :: String -> [String]
+paramsForCabal tixFile = ["report", tixFile, "--include=Numeric.Euler.Primes"]
+
+paramsForRunAH :: String -> [String]
+paramsForRunAH tixFile = ["report", tixFile, "--hpcdir=dist/hpc/mix/spec", "--hpcdir=dist/hpc/mix/euler-0.8.0", "--exclude=Main"]
+
 usefulMetrics :: QName -> Bool
 usefulMetrics name = name `elem` [
   simpleQName "exprs",
@@ -36,11 +42,12 @@ coverageOk :: [Float] -> Bool
 coverageOk values = (realToFrac (sum values) / genericLength values) > (expected :: Double)
 --coverageOk = all (> expected)
 
-hpc :: String -> Bool -> IO String
-hpc tixFile True  = readProcess "hpc" ["report", tixFile, "--include=Numeric.Euler.Primes", "--xml-output"] ""
-hpc tixFile False = readProcess "hpc" ["report", tixFile, "--include=Numeric.Euler.Primes"] ""
+hpc :: [String] -> Bool -> IO String
+hpc tixFile True  = readProcess "hpc" (tixFile ++ ["--xml-output"]) ""
+hpc tixFile False = readProcess "hpc" tixFile ""
 
-readCoverageFrom :: String -> IO ()
+
+readCoverageFrom :: [String] -> IO ()
 readCoverageFrom tixFile = do
   s <- hpc tixFile True
   case parseXMLDoc s of 
@@ -58,6 +65,6 @@ main :: IO ()
 main = do
   specExists <- doesFileExist "./spec.tix"
   if specExists 
-  then readCoverageFrom ("./spec.tix" :: String)
-  else readCoverageFrom ("./dist/hpc/tix/spec/spec.tix" :: String)
+  then readCoverageFrom (paramsForCabal "./spec.tix")
+  else readCoverageFrom (paramsForRunAH "./dist/hpc/tix/spec/spec.tix")
 
