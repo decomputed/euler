92da55f8f3186624d60853326a3472e693999c36
Luis <luis@decomputed.com>
2015-03-19 08:55:46 +0100

    Logs debugging information so I can understand why this fails in nixOS builds.

--- test/HPC.hs
+++ test/HPC.hs
@@ -17,7 +17,7 @@ paramsForCabal :: String -> [String]
 paramsForCabal tixFile = ["report", tixFile, "--include=Numeric.Euler.Primes"]
 
 paramsForRunAH :: String -> [String]
-paramsForRunAH tixFile = ["report", tixFile, "--hpcdir=dist/hpc/mix/spec", "--hpcdir=dist/hpc/mix/euler-0.8.0", "--exclude=Main"]
+paramsForRunAH tixFile = ["report", tixFile, "--hpcdir=dist/hpc/mix/spec", "--exclude=Main"]
 
 usefulMetrics :: QName -> Bool
 usefulMetrics name = name `elem` [
@@ -42,29 +42,70 @@ coverageOk :: [Float] -> Bool
 coverageOk values = (realToFrac (sum values) / genericLength values) > (expected :: Double)
 --coverageOk = all (> expected)
 
-hpc :: [String] -> Bool -> IO String
-hpc tixFile True  = readProcess "hpc" (tixFile ++ ["--xml-output"]) ""
-hpc tixFile False = readProcess "hpc" tixFile ""
+hpc :: [String] -> Bool -> IO (ExitCode, String, String)
+hpc tixFile True  = readProcessWithExitCode "hpc" (tixFile ++ ["--xml-output"]) ""
+hpc tixFile False = readProcessWithExitCode "hpc" tixFile ""
 
+printProcessOutput :: (ExitCode,String,String) -> IO ()
+printProcessOutput (exitCode,stdout,stderr) = do
+  putStrLn (show exitCode)
+  putStrLn "......................................."
+  putStrLn stdout
+  putStrLn "......................................."
+  putStrLn stderr
 
 readCoverageFrom :: [String] -> IO ()
 readCoverageFrom tixFile = do
-  s <- hpc tixFile True
-  case parseXMLDoc s of 
+  (exitCode,stdout,stderr) <- hpc tixFile True
+  putStrLn ""
+  putStrLn "2 ====== Output from hpc command ======"
+  printProcessOutput (exitCode,stdout,stderr)
+  putStrLn "======================================="
+  putStrLn ""
+  case parseXMLDoc stdout of 
    Nothing -> error "Failed to parse xml. Try running HPC manually..."
    Just doc -> let elements = filterChildrenName usefulMetrics (head $ elChildren doc)
                    values = map extractValues elements
                    percents = map calculatePercentages values
                in if coverageOk percents
-                  then exitSuccess
+                  then do
+                    putStrLn "3 ====== Coverage was OK. Exiting with success."
+                    putStrLn ""
+                    exitSuccess
                   else do
-                    hpcOutput <- hpc tixFile False
-                    putStr hpcOutput >> exitFailure
+                    putStrLn ("3 ====== Coverage was NOT ok:" ++ show ((realToFrac (sum percents) / genericLength percents)::Double))
+                    (exitCode2,stdout2,stderr2) <- hpc tixFile False
+                    putStrLn "3 ====== Output from hpc command ======"
+                    printProcessOutput (exitCode2,stdout2,stderr2)
+                    putStrLn "======================================="
+                    putStrLn ""
+                    exitFailure
+
+fileExists :: String -> IO Bool
+fileExists fileName =
+  do
+    specExists <- doesFileExist fileName
+    if specExists
+      then
+      do
+        putStrLn ("1 ====== '" ++ fileName ++ "' EXISTS.")
+        return True
+      else
+      do
+        putStrLn ("1 ====== '" ++ fileName ++ "' does NOT exist.")
+        return False
 
 main :: IO ()
-main = do
-  specExists <- doesFileExist "./spec.tix"
-  if specExists 
-  then readCoverageFrom (paramsForCabal "./spec.tix")
-  else readCoverageFrom (paramsForRunAH "./dist/hpc/tix/spec/spec.tix")
+main =
+  do
+    putStrLn ""
+    exists <- fileExists "./spec.tix"
+    if exists
+    then readCoverageFrom (paramsForCabal "./spec.tix")
+    else 
+      do
+        exists2 <- fileExists "./dist/hpc/tix/spec/spec.tix"
+        if exists2
+        then readCoverageFrom (paramsForRunAH "./dist/hpc/tix/spec/spec.tix")
+        else putStrLn "1 ====== No file exists." >> exitFailure
 
