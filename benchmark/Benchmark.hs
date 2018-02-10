4bf3abbc7b3da63e5f539ca678c089acb38f62ae
Luis Rodrigues Soares <luis@decomputed.com>
2018-02-10 22:35:35 +0100

    Move Haskell code to Haskell branch.

--- benchmark/Benchmark.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import Criterion.Main (bgroup, defaultMain)
-import Numeric.Euler.PrimesBenchmark
-
-main :: IO ()
-main = defaultMain
-       [ bgroup "Primes" benchmarks
-       ]
